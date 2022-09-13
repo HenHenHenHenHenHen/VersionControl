bd6f0cee31e7b1cfc92f6d99d753058e355ebf58
Henry <henrydorff@gmail.com>
2022-09-12 20:26:25 -0700

    Created JUnit testers (empty as of now)

---
+++ src/JoshBlobTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JoshBlobTest {
+
+	@Test
+	public void testBlob() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncryptThisString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFileName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompressFile() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/JoshIndexTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JoshIndexTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIndex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+}
