991e6a3be637a3333400f3ee28421ab5d6373669
Henry <henrydorff@gmail.com>
2022-09-15 22:36:10 -0700

    JUnitTesterWorks (probably)

--- src/JoshIndexTest.java
+++ src/JoshIndexTest.java
@@ -12,6 +12,28 @@ import org.junit.Test;
 import git.Index;
 
 public class JoshIndexTest {
+	public String readFile (String fileName) {
+		Scanner testScanner = new Scanner ("test/" + fileName);
+		StringBuilder newString = new StringBuilder();
+		while (testScanner.hasNextLine()) {
+			newString.append(testScanner.nextLine());
+		}
+		return newString.toString();
+	}
+	
+	public String read1Index () { 
+		Scanner testScanner = new Scanner ("test/Index.txt");
+		String firstLine = testScanner.nextLine();
+		return firstLine;
+	}
+	
+	public String read2Index () { 
+		Scanner testScanner = new Scanner ("test/Index.txt");
+		testScanner.nextLine();
+		String secondLine = testScanner.nextLine();
+		return secondLine;
+	}
+	
 	@Test
 	public void testInit() throws IOException {
 		File deleteObjectsFolder = new File ("test/objects");
@@ -22,17 +44,9 @@ public class JoshIndexTest {
 		Index indexTest = new Index();
 		indexTest.init();
 		File objectsFolder = new File ("test/objects");
-		if (!objectsFolder.isDirectory()) {
-			fail ("objects folder does not exist");
-		} 
+		assertTrue ("objects folder does not exist", objectsFolder.isDirectory());
 		File indexFile = new File ("test/index.txt");
-		if (!indexFile.isFile()) {
-			fail ("index.txt does not exist");
-		}
-		
-		
-		
-		
+		assertTrue ("index.txt does not exist", indexFile.isFile());
 	
 	}
 
@@ -53,23 +67,30 @@ public class JoshIndexTest {
 		indexTest.add("test2.txt");
 		
 		File objectsFolder = new File ("test/objects");
-		if (objectsFolder.isDirectory() && objectsFolder.list().length != 2) {
-			System.out.print("objects folder not updated with Blobs");
-			fail ("objects folder not updated with Blobs");
-		}
+		assertTrue ("objects folder does not contain both blobs", objectsFolder.isDirectory() && objectsFolder.list().length != 2);
+			String test1Contents = readFile ("test1.txt");
+			String objectsTest1Contents = readFile ("objects/" + SHA1.encryptThisString(test1Contents));
+			assertTrue ("1st tested object did not match SHAString or was otherwise incorrectly named", objectsTest1Contents.equals("some content") || objectsTest1Contents.equals(SHA1.encryptThisString(test1Contents)));
+			
+			String test2Contents = readFile ("test2.txt");
+			String objectsTest2Contents = readFile ("objects/" + SHA1.encryptThisString(test2Contents));
+			assertTrue ("2nd tested object did not match SHAString or was otherwise incorrectly named", objectsTest2Contents.equals("some content") || objectsTest2Contents.equals(SHA1.encryptThisString(test2Contents)));
 		
-		Scanner indexFileScanner = new Scanner ("test/index.txt");
+		Scanner indexFileScanner = new Scanner ("test/Index.txt");
 		int numOfLines = 0;
 		while (indexFileScanner.hasNextLine()) {
 			indexFileScanner.nextLine();
 			numOfLines++;
 		}
 		indexFileScanner.close();
-		if (numOfLines != 2) {
-			System.out.print("index.txt does not contain both file indexes");
-			fail ("index.txt does not contain both file indexes");
+		assertTrue ("index.txt does not contain both file indexes", numOfLines == 2);
+			String index1 = read1Index();
+			assertTrue ("index.txt first entry not correct", index1.equals("test1.txt : " + SHA1.encryptThisString("some content")));
+			
+			String index2 = read2Index();
+			assertTrue ("index.txt second entry not correct", index2.equals("test2.txt : " + SHA1.encryptThisString("some more content")));
 		}
-	}
+		
 
 	@Test
 	public void testRemove() throws IOException {
@@ -90,20 +111,19 @@ public class JoshIndexTest {
 		indexTest.remove("test2.txt");
 		
 		File objectsFolder = new File ("test/objects");
-		if (objectsFolder.isDirectory() && objectsFolder.list().length != 1) {
-			fail ("did not delete Blob");
-		}
+		assertTrue ("did not delete Blob", objectsFolder.isDirectory() && objectsFolder.list().length == 1);
+
 		
-		Scanner indexFileScanner = new Scanner ("test/index.txt");
+		Scanner indexFileScanner = new Scanner ("test/Index.txt");
 		int numOfLines = 0;
 		while (indexFileScanner.hasNextLine()) {
 			indexFileScanner.nextLine();
 			numOfLines++;
 		}
 		indexFileScanner.close();
-		if (numOfLines != 1) {
-			fail ("did not remove an index in index.txt");
+		assertTrue ("did not remove an index in index.txt", numOfLines == 1);
+			String index1 = read1Index();
+			assertTrue ("first entry in index is no longer around", index1.equals("test1.txt : " + SHA1.encryptThisString("test1.txt")));
 		}
 	}
 
-}
