221768b4c6f8fb4d358e8809e3d47e3fbab5b17d
Joshua Cheng <joshuacheng888@gmail.com>
2022-09-20 00:11:44 -0700

    Commits now link correctly - child and parent

--- src/git/Commit.java
+++ src/git/Commit.java
@@ -1,6 +1,7 @@
 package git;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -15,38 +16,92 @@ public class Commit {
 	String summary;
 	String author;
 	String date;
-	String prev = null;
-	String next = null;
-	public Commit(String pTree, String summary,String author, String parentPointer) throws FileNotFoundException
+	Commit parent ;
+	Commit child;
+	String sha1Hash;
+	String parentSha1Hash;
+	String childSha1Hash;
+	boolean isHead;
+	public Commit(String pTree, String summary,String author, Commit parent) throws FileNotFoundException
 	{
-		
+		if (parent == null)
+		{
+			isHead = true;
+		}
+		else
+			isHead = false;
+		child = null;
 		this.pTree = pTree;
-		this.prev = parentPointer;
-		
 		this.summary = summary;
 		this.author = author;
 		this.date = getDate();
+		
+		this.parent = parent;
+	
+	
+		if (this.parent == null)
+		{
+			parentSha1Hash = null;
+		}
+		else {
+	
+			parentSha1Hash = "test/objects/" + parent.getCommitHash();
+			setParent();
+		}
+	
+		
+		//generates the sha1 based on contents of commit
+		sha1Hash = encryptThisString(getSubsetFileContents());
+		
+		
 		writeCommitFile();
 		
 	}
 	
-	public String subsetFileContents()
+	public String getSubsetFileContents()
 	{
-		return summary+ "\n" + date + "\n" + author + "\n" + prev;
+		return summary+ "\n" + date + "\n" + author + "\n" + parentSha1Hash;
 	}
 	
+	public String getCommitHash()
+	{
+		sha1Hash = encryptThisString(getSubsetFileContents());
+		return sha1Hash;
+	}
 	public void writeCommitFile() throws FileNotFoundException
 	{
-		PrintWriter pw = new PrintWriter("test/objects/" + encryptThisString(subsetFileContents()));
+		if (child == null)
+		{
+			childSha1Hash = null;
+		}
+		else {
+			childSha1Hash = "test/objects/" + child.getCommitHash();
+			System.out.println("doasfjsOK" +  child);
+		}
+		PrintWriter pw = new PrintWriter("test/objects/" + sha1Hash);
 		pw.append(pTree + "\n");
-		pw.append(prev + "\n");
-		pw.append(next + "\n");
+		pw.append(parentSha1Hash + "\n");
+		pw.append(childSha1Hash + "\n");
 		pw.append(author + "\n");
 		pw.append(date + "\n");
 		pw.append(summary);
 		pw.close();
 	}
+	private void setParent () throws FileNotFoundException{
+			parent.setChild(this);	
+			parent.writeCommitFile();
 	
+	}
+	
+	private void setChild (Commit child) {
+		this.child = child;
+	//	System.out.println("i ran");
+	}
+	
+	
+	
+	
+	//sha 1 creator
 	public static String encryptThisString(String input)
 	{
 		try {	         
@@ -64,7 +119,7 @@ public class Commit {
 			throw new RuntimeException(e);
 		}
 	}
-	
+	//gets the date duh
 	public String getDate()
 	{
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
@@ -73,10 +128,12 @@ public class Commit {
 	}
 	public static void main(String[] args) throws FileNotFoundException {
 		
-//		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-//		String data = df.format(new Date());
-//		System.out.println(data);
-		Commit theMit = new Commit(null,"yuh","Jbao","test/objects/1dc0eea45513abfb82439a406563352f1adc697e");
+		Commit c1 = new Commit ("someTree", "first commit", "JBao", null);
+		Commit c2 = new Commit ("somTree2", "WEEEEE commit", "JBao", c1);
+		System.out.println("first commit child is" + c1.childSha1Hash);
+		Commit c3 = new Commit ("someTree3", "good mesure", "JBAO", c2);
+		
+		
 	}
 
 }
